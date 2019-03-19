80a768348978815f6b1ae3f5c181cf80ae95beb1
Michelle Ip <mvi4@cornell.edu>
2019-03-18 23:04:21 -0400

    Added stuff from steps 1 & 2 from polar bear tutorial

---
+++ deploy.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+var iothub = require('azure-iothub');
+var registry = iothub.Registry.fromConnectionString('HostName=cowzure-hub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=2mVpZEzsuZlpikWxlfdhU3EsQNtlcAEw6i9PI1FgcqE=');
+
+console.log('Reading devices.json...');
+var devices = JSON.parse(fs.readFileSync('devices.json', 'utf8'));
+
+console.log('Registering devices...');
+registry.addDevices(devices, (err, info, res) => {
+    registry.list((err, info, res) => {
+        info.forEach(device => {
+            devices.find(o => o.deviceId === device.deviceId).key = device.authentication.symmetricKey.primaryKey;          
+        });
+
+        console.log('Writing sensors.json...');
+        fs.writeFileSync('sensors.json', JSON.stringify(devices, null, 4), 'utf8');
+        console.log('Done');
+    });
+});
